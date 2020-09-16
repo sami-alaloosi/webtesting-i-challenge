0859d998a111d1d3ba928f45373285e9dbd8c7ce
sami-aloosi <kingxsam@gmail.com>
2020-09-16 16:45:33 -0700

    done

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,17 +6,23 @@ module.exports = {
 };
 
 function success(item) {
-  return { ...item };
+  return { ...item, enhancement: item.enhancement < 20? ++item.enhancement : item.enhancement };
 }
 
 function fail(item) {
-  return { ...item };
+  if ( item.enhancement < 15) {
+    return { ...item, durability: item.durability -5   }
+  } else if (item.enhancement >= 15 && item.enhancement <= 16) {
+    return { ...item, durability: item.durability -10   }
+  } else { return { ...item, enhancement: --item.enhancement  }}
 }
 
 function repair(item) {
-  return { ...item };
+  return { ...item, durability: 100 };
 }
 
 function get(item) {
   return { ...item };
 }
+
+
